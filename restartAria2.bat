525d4cc92484d02041f845dc5e4b584f7a53955d
ewigl <ewiglichtl@163.com>
2023-12-21 16:11:43 +0800

    update restart script

--- restartAria2.bat
+++ restartAria2.bat
@@ -2,19 +2,26 @@
 
 SET PROCESS_NAME=aria2c.exe
 
-echo Are U sure to restart aria2 ?
+echo Restart aria2 ?
 pause
 
-@REM taskkill /F /T /im %PROCESS_NAME%
-taskkill /F /im %PROCESS_NAME%
-
-start aria2.vbs
+tasklist | findstr "%PROCESS_NAME%"
+if ERRORLEVEL 1 (
+    echo Aria2 not running, Starting...
+) else (
+    echo Aria2 running, Killing...
+    taskkill /F /T /im %PROCESS_NAME%
+)
 
 echo Restarting...
+start aria2.vbs
 timeout /t 2
 
-tasklist | find "%PROCESS_NAME%"
-
-echo Aria2 Restarted.
+tasklist | findstr "%PROCESS_NAME%"
+if ERRORLEVEL 1 (
+    echo Aria2 restart failed.
+) else (
+    echo Aria2 Restarted.
+)
 
 pause
